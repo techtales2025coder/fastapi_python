42928055a55cf2e5b76baa2e4873f8d100abf99c
techtales2025coder <techtales1025@gmail.com>
2025-10-17 22:08:42 -0700

    Add reservations pydantic models

---
+++ models/reservations.py
@@ -0,0 +1,15 @@
+from typing import Union
+from pydantic import BaseModel
+from datetime import datetime
+
+class User(BaseModel):
+    id: int
+    first_name: str
+    second_name: str
+
+class ParkingSpot(BaseModel):
+    id: Union[int, None] = None
+    reserved_at: Union[datetime, None] = None
+    expires_at: Union[datetime, None] = None
+    reserved_by: User
+
